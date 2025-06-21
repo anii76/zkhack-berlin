67db185860478d7a7e71153aa5fed5e6a36038a9
Nate Faber <nathaniel@njfaber.com>
2025-06-22 00:20:59 +0200

    keep working to deploy with new data types

--- test/ZFace.test.ts
+++ test/ZFace.test.ts
@@ -1,17 +1,26 @@
 import { expect } from "chai";
 import hre, { ethers } from "hardhat";
+import { HonkVerifier } from "../typechain-types";
+import { BytesLike } from "ethers";
+
+const faceA = Array.from({ length: 128 }, (_, i) => i / 127);
+const faceABytes: BytesLike[] = faceA.map((x) => ethers.hexlify(x);
+const faceB = faceA.map((x) => 1 - x);
+
+let verifierContract: HonkVerifier;
+before(async () => {
+  verifierContract = await ethers.deployContract("HonkVerifier");
+})
 
 it("proves and verifies on-chain", async () => {
   // Deploy a verifier contract
-  const contractFactory = await ethers.getContractFactory("MyContract");
-  const contract = await contractFactory.deploy();
+  const contractFactory = await ethers.getContractFactory("ZFace");
+  const contract = await contractFactory.deploy(await verifierContract.getAddress(), faceA);
   await contract.waitForDeployment();
 
   // Generate a proof
   const { noir, backend } = await hre.noir.getCircuit("my_noir");
 
-  const faceA = Array.from({ length: 128 }, (_, i) => i / 127);
-  const faceB = faceA.map((x) => 1 - x);
 
   const input = { probeFace: faceA, referenceFace: faceA };
   const { witness } = await noir.execute(input);
