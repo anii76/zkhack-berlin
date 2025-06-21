e1d9fee1c4b1cdc3ac1b1cb5579bffda150819f7
Nate Faber <nathaniel@njfaber.com>
2025-06-21 23:50:27 +0200

    work on rewriting

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -3,15 +3,22 @@ pragma solidity ^0.8.27;
 
 import {HonkVerifier} from "../noir/target/my_noir.sol";
 
-contract MyContract {
-    HonkVerifier public verifier = new HonkVerifier();
+contract ZFace {
+    HonkVerifier public verifier;
+    bytes32[128] public faceEncoding;
+
+    constructor(address _verifier, bytes32[128] memory _faceEncoding) {
+        verifier = HonkVerifier(_verifier);
+        faceEncoding = _faceEncoding;
+    }
 
     function verify(
-        bytes calldata proof,
-        uint256 y
+        bytes calldata proof
     ) external view returns (bool) {
-        bytes32[] memory publicInputs = new bytes32[](1);
-        publicInputs[0] = bytes32(y);
+        bytes32[] memory publicInputs = new bytes32[](128);
+        for (uint256 i = 0; i < 128; i++) {
+            publicInputs[i] = faceEncoding[i];
+        }
         bool result = verifier.verify(proof, publicInputs);
         return result;
     }
