39b9030cbe3b86e1b244c0df29bed666935a0774
Nate Faber <nathaniel@njfaber.com>
2025-06-21 23:50:27 +0200

    work on rewriting

--- test/MyContract.test.ts
+++ test/MyContract.test.ts
@@ -9,16 +9,21 @@ it("proves and verifies on-chain", async () => {
 
   // Generate a proof
   const { noir, backend } = await hre.noir.getCircuit("my_noir");
-  const input = { x: 1, y: 2 };
+
+  const faceA = Array.from({ length: 128 }, (_, i) => i / 127);
+  const faceB = faceA.map((x) => 1 - x);
+
+  const input = { probeFace: faceA, referenceFace: faceA };
   const { witness } = await noir.execute(input);
   const { proof, publicInputs } = await backend.generateProof(witness, {
     keccak: true,
   });
+  console.debug({ proof })
   // it matches because we marked y as `pub` in `main.nr`
-  expect(BigInt(publicInputs[0])).to.eq(BigInt(input.y));
+  // expect(BigInt(publicInputs[30] * 127)).to.eq(BigInt(input.referenceFace[30] * 127));
 
   // Verify the proof on-chain
-  const result = await contract.verify(proof, input.y);
+  const result = await contract.verify(proof);
   expect(result).to.eq(true);
 
   // You can also verify in JavaScript.
