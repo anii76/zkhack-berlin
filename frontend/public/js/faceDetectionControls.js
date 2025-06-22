21b4cedc8e6c8f4f7db77d54f4f1661ed113fd6b
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2025-06-22 09:00:10 +0200

    typescript support added

--- frontend/public/js/faceDetectionControls.js
+++ frontend/public/js/faceDetectionControls.js
@@ -1,115 +1,105 @@
-const SSD_MOBILENETV1 = 'ssd_mobilenetv1'
-const TINY_FACE_DETECTOR = 'tiny_face_detector'
-
-
-let selectedFaceDetector = SSD_MOBILENETV1
-
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const SSD_MOBILENETV1 = 'ssd_mobilenetv1';
+const TINY_FACE_DETECTOR = 'tiny_face_detector';
+let selectedFaceDetector = SSD_MOBILENETV1;
 // ssd_mobilenetv1 options
-let minConfidence = 0.5
-
+let minConfidence = 0.5;
 // tiny_face_detector options
-let inputSize = 512
-let scoreThreshold = 0.5
-
+let inputSize = 512;
+let scoreThreshold = 0.5;
 function getFaceDetectorOptions() {
-  return selectedFaceDetector === SSD_MOBILENETV1
-    ? new faceapi.SsdMobilenetv1Options({ minConfidence })
-    : new faceapi.TinyFaceDetectorOptions({ inputSize, scoreThreshold })
+    return selectedFaceDetector === SSD_MOBILENETV1
+        ? new faceapi.SsdMobilenetv1Options({ minConfidence })
+        : new faceapi.TinyFaceDetectorOptions({ inputSize, scoreThreshold });
 }
-
 function onIncreaseMinConfidence() {
-  minConfidence = Math.min(faceapi.utils.round(minConfidence + 0.1), 1.0)
-  $('#minConfidence').val(minConfidence)
-  updateResults()
+    minConfidence = Math.min(faceapi.utils.round(minConfidence + 0.1), 1.0);
+    $('#minConfidence').val(minConfidence);
+    updateResults();
 }
-
 function onDecreaseMinConfidence() {
-  minConfidence = Math.max(faceapi.utils.round(minConfidence - 0.1), 0.1)
-  $('#minConfidence').val(minConfidence)
-  updateResults()
+    minConfidence = Math.max(faceapi.utils.round(minConfidence - 0.1), 0.1);
+    $('#minConfidence').val(minConfidence);
+    updateResults();
 }
-
 function onInputSizeChanged(e) {
-  changeInputSize(e.target.value)
-  updateResults()
+    changeInputSize(e.target.value);
+    updateResults();
 }
-
 function changeInputSize(size) {
-  inputSize = parseInt(size)
-
-  const inputSizeSelect = $('#inputSize')
-  inputSizeSelect.val(inputSize)
-  inputSizeSelect.material_select()
+    inputSize = parseInt(size);
+    const inputSizeSelect = $('#inputSize');
+    inputSizeSelect.val(inputSize);
+    inputSizeSelect.material_select();
 }
-
 function onIncreaseScoreThreshold() {
-  scoreThreshold = Math.min(faceapi.utils.round(scoreThreshold + 0.1), 1.0)
-  $('#scoreThreshold').val(scoreThreshold)
-  updateResults()
+    scoreThreshold = Math.min(faceapi.utils.round(scoreThreshold + 0.1), 1.0);
+    $('#scoreThreshold').val(scoreThreshold);
+    updateResults();
 }
-
 function onDecreaseScoreThreshold() {
-  scoreThreshold = Math.max(faceapi.utils.round(scoreThreshold - 0.1), 0.1)
-  $('#scoreThreshold').val(scoreThreshold)
-  updateResults()
+    scoreThreshold = Math.max(faceapi.utils.round(scoreThreshold - 0.1), 0.1);
+    $('#scoreThreshold').val(scoreThreshold);
+    updateResults();
 }
-
 function onIncreaseMinFaceSize() {
-  minFaceSize = Math.min(faceapi.utils.round(minFaceSize + 20), 300)
-  $('#minFaceSize').val(minFaceSize)
+    minFaceSize = Math.min(faceapi.utils.round(minFaceSize + 20), 300);
+    $('#minFaceSize').val(minFaceSize);
 }
-
 function onDecreaseMinFaceSize() {
-  minFaceSize = Math.max(faceapi.utils.round(minFaceSize - 20), 50)
-  $('#minFaceSize').val(minFaceSize)
+    minFaceSize = Math.max(faceapi.utils.round(minFaceSize - 20), 50);
+    $('#minFaceSize').val(minFaceSize);
 }
-
 function getCurrentFaceDetectionNet() {
-  if (selectedFaceDetector === SSD_MOBILENETV1) {
-    return faceapi.nets.ssdMobilenetv1
-  }
-  if (selectedFaceDetector === TINY_FACE_DETECTOR) {
-    return faceapi.nets.tinyFaceDetector
-  }
+    if (selectedFaceDetector === SSD_MOBILENETV1) {
+        return faceapi.nets.ssdMobilenetv1;
+    }
+    if (selectedFaceDetector === TINY_FACE_DETECTOR) {
+        return faceapi.nets.tinyFaceDetector;
+    }
 }
-
 function isFaceDetectionModelLoaded() {
-  return !!getCurrentFaceDetectionNet().params
+    return !!getCurrentFaceDetectionNet().params;
 }
-
-async function changeFaceDetector(detector) {
-  ['#ssd_mobilenetv1_controls', '#tiny_face_detector_controls']
-    .forEach(id => $(id).hide())
-
-  selectedFaceDetector = detector
-  const faceDetectorSelect = $('#selectFaceDetector')
-  faceDetectorSelect.val(detector)
-  faceDetectorSelect.material_select()
-
-  $('#loader').show()
-  if (!isFaceDetectionModelLoaded()) {
-    await getCurrentFaceDetectionNet().load('/')
-  }
-
-  $(`#${detector}_controls`).show()
-  $('#loader').hide()
+function changeFaceDetector(detector) {
+    return __awaiter(this, void 0, void 0, function* () {
+        ['#ssd_mobilenetv1_controls', '#tiny_face_detector_controls']
+            .forEach(id => $(id).hide());
+        selectedFaceDetector = detector;
+        const faceDetectorSelect = $('#selectFaceDetector');
+        faceDetectorSelect.val(detector);
+        faceDetectorSelect.material_select();
+        $('#loader').show();
+        if (!isFaceDetectionModelLoaded()) {
+            yield getCurrentFaceDetectionNet().load('/');
+        }
+        $(`#${detector}_controls`).show();
+        $('#loader').hide();
+    });
 }
-
-async function onSelectedFaceDetectorChanged(e) {
-  selectedFaceDetector = e.target.value
-
-  await changeFaceDetector(e.target.value)
-  updateResults()
+function onSelectedFaceDetectorChanged(e) {
+    return __awaiter(this, void 0, void 0, function* () {
+        selectedFaceDetector = e.target.value;
+        yield changeFaceDetector(e.target.value);
+        updateResults();
+    });
 }
-
 function initFaceDetectionControls() {
-  const faceDetectorSelect = $('#selectFaceDetector')
-  faceDetectorSelect.val(selectedFaceDetector)
-  faceDetectorSelect.on('change', onSelectedFaceDetectorChanged)
-  faceDetectorSelect.material_select()
-
-  const inputSizeSelect = $('#inputSize')
-  inputSizeSelect.val(inputSize)
-  inputSizeSelect.on('change', onInputSizeChanged)
-  inputSizeSelect.material_select()
+    const faceDetectorSelect = $('#selectFaceDetector');
+    faceDetectorSelect.val(selectedFaceDetector);
+    faceDetectorSelect.on('change', onSelectedFaceDetectorChanged);
+    faceDetectorSelect.material_select();
+    const inputSizeSelect = $('#inputSize');
+    inputSizeSelect.val(inputSize);
+    inputSizeSelect.on('change', onInputSizeChanged);
+    inputSizeSelect.material_select();
 }
--- frontend/public/js/imageSelectionControls.js
+++ frontend/public/js/imageSelectionControls.js
@@ -1,63 +1,56 @@
-async function onSelectedImageChanged(uri) {
-  const img = await faceapi.fetchImage(uri)
-  $(`#inputImg`).get(0).src = img.src
-  updateResults()
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function onSelectedImageChanged(uri) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const img = yield faceapi.fetchImage(uri);
+        $(`#inputImg`).get(0).src = img.src;
+        updateResults();
+    });
 }
-
-async function loadImageFromUrl(url) {
-  const img = await requestExternalImage($('#imgUrlInput').val())
-  $('#inputImg').get(0).src = img.src
-  updateResults()
+function loadImageFromUrl(url) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const img = yield requestExternalImage($('#imgUrlInput').val());
+        $('#inputImg').get(0).src = img.src;
+        updateResults();
+    });
 }
-
-async function loadImageFromUpload() {
-    const imgFile = $('#queryImgUploadInput').get(0).files[0]
-    const img = await faceapi.bufferToImage(imgFile)
-    $('#inputImg').get(0).src = img.src
-    updateResults()
+function loadImageFromUpload() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const imgFile = $('#queryImgUploadInput').get(0).files[0];
+        const img = yield faceapi.bufferToImage(imgFile);
+        $('#inputImg').get(0).src = img.src;
+        updateResults();
+    });
 }
-
 function renderImageSelectList(selectListId, onChange, initialValue, withFaceExpressionImages) {
-  let images = [1, 2, 3, 4, 5].map(idx => `bbt${idx}.jpg`)
-
-  if (withFaceExpressionImages) {
-    images = [
-      'happy.jpg',
-      'sad.jpg',
-      'angry.jpg',
-      'disgusted.jpg',
-      'surprised.jpg',
-      'fearful.jpg',
-      'neutral.jpg'
-    ].concat(images)
-  }
-
-  function renderChildren(select) {
-    images.forEach(imageName =>
-      renderOption(
-        select,
-        imageName,
-        imageName
-      )
-    )
-  }
-
-  renderSelectList(
-    selectListId,
-    onChange,
-    initialValue,
-    renderChildren
-  )
+    let images = [1, 2, 3, 4, 5].map(idx => `bbt${idx}.jpg`);
+    if (withFaceExpressionImages) {
+        images = [
+            'happy.jpg',
+            'sad.jpg',
+            'angry.jpg',
+            'disgusted.jpg',
+            'surprised.jpg',
+            'fearful.jpg',
+            'neutral.jpg'
+        ].concat(images);
+    }
+    function renderChildren(select) {
+        images.forEach(imageName => renderOption(select, imageName, imageName));
+    }
+    renderSelectList(selectListId, onChange, initialValue, renderChildren);
 }
-
 function initImageSelectionControls(initialValue = 'bbt1.jpg', withFaceExpressionImages = false) {
-  renderImageSelectList(
-    '#selectList',
-    async (uri) => {
-      await onSelectedImageChanged(uri)
-    },
-    initialValue,
-    withFaceExpressionImages
-  )
-  onSelectedImageChanged($('#selectList select').val())
+    renderImageSelectList('#selectList', (uri) => __awaiter(this, void 0, void 0, function* () {
+        yield onSelectedImageChanged(uri);
+    }), initialValue, withFaceExpressionImages);
+    onSelectedImageChanged($('#selectList select').val());
 }
