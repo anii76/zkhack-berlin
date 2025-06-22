c6ef007ce1a3f94b4aa0f25cd99c830eb8d744f7
anfal.bourouina <anfal.bourouina@qonto.com>
2025-06-22 05:47:05 +0200

    adjust stuff

--- backend/ignition/modules/Starter.ts
+++ backend/ignition/modules/Starter.ts
@@ -2,7 +2,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const StarterModule = buildModule("ZFace", (m) => {
 
-  const verifier = m.contract("noir/target/my_noir.sol:HonkVerifier");
+  const verifier = m.contract("noir/target/zface_verifier.sol:HonkVerifier");
   
   // Create a bytes32[128] array for face encoding (all zeros for now)
   const faceEncoding = Array(128).fill("0x0000000000000000000000000000000000000000000000000000000000000000");
