6a614341d3971b4aebf489b70c713eec4980c8ea
anfal.bourouina <anfal.bourouina@qonto.com>
2025-06-22 05:51:53 +0200

    add deployer

--- backend/contracts/ZFace.sol
+++ backend/contracts/ZFace.sol
@@ -27,4 +27,42 @@ contract ZFace {
         bool result = verifier.verify(proof, publicInputs);
         return result;
     }
+    
+    receive() external payable {}
+
+    function withdraw(address payable to, bytes calldata proof) external {
+        require(verify(proof), "Not authorized"); // require reciever to prove identity using face 
+        selfdestruct(to);
+    }
+}
+
+
+contract Deployer {
+    event Deployed(address addr);
+
+    function getAddress(bytes32 salt, address owner, bytes32[128] memory faceEncoding) external view returns (address) {
+        bytes memory bytecode = getBytecode(owner, faceEncoding);
+        bytes32 hash = keccak256(
+            abi.encodePacked(bytes1(0xff), address(this), salt, keccak256(bytecode))
+        );
+        return address(uint160(uint256(hash)));
+    }
+
+    function deploy(bytes32 salt, address owner, bytes32[128] memory faceEncoding) external payable returns (address addr) {
+        bytes memory bytecode = getBytecode(owner, faceEncoding);
+        assembly {
+            addr := create2(callvalue(), add(bytecode, 0x20), mload(bytecode), salt)
+            if iszero(extcodesize(addr)) {
+                revert(0, 0)
+            }
+        }
+        emit Deployed(addr);
+    }
+
+    function getBytecode(address owner, bytes32[128] memory faceEncoding) public pure returns (bytes memory) {
+        return abi.encodePacked(
+            type(ZFace).creationCode,
+            abi.encode(owner, faceEncoding)
+        );
+    }
 }
