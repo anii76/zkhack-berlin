784317a39c47a4d431e26838187e3afa4372ece2
anfal.bourouina <anfal.bourouina@qonto.com>
2025-06-22 05:53:03 +0200

    add hardhat config

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -10,6 +10,31 @@ const config: HardhatUserConfig = {
   noir: {
     version: "v0.39.0",
   },
+  defaultNetwork: "arbitrumSepolia",//"hardhat",
+  networks: {
+    arbitrumSepolia: {
+      url: "https://sepolia-rollup.arbitrum.io/rpc",
+      accounts: [""]
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+      chainId: 31337
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  etherscan: {
+    // Your API key for Etherscan _
+    // Obtain one at https://etherscan.io/ or https://polygonscan.com/
+    apiKey: { arbitrumSepolia: "" }
+  }
 };
 
 export default config;
+
