4c7362d59fbd248097f9dca9754a68cdafb3907b
anfal.bourouina <anfal.bourouina@qonto.com>
2025-06-22 05:47:05 +0200

    adjust stuff

--- backend/ignition/modules/Verifier.ts
+++ backend/ignition/modules/Verifier.ts
@@ -2,7 +2,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const VerifierModule = buildModule("HonkVerifier", (m) => {
 
-  const verifier = m.contract("noir/target/my_noir.sol:HonkVerifier");
+  const verifier = m.contract("noir/target/zface_verifier.sol:HonkVerifier");
 
 
   return { verifier };
