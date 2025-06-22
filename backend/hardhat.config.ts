87b86f20b0479d871bfb862732a4c31df0439ce6
anfal.bourouina <anfal.bourouina@qonto.com>
2025-06-22 09:07:32 +0200

    get sender embeddings

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,6 +1,10 @@
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-noir";
 import { HardhatUserConfig } from "hardhat/config";
+import * as dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -13,8 +17,8 @@ const config: HardhatUserConfig = {
   defaultNetwork: "arbitrumSepolia",//"hardhat",
   networks: {
     arbitrumSepolia: {
-      url: "https://sepolia-rollup.arbitrum.io/rpc",
-      accounts: ["259729b49f1748d2842867a8f3871fe81f8ff14878b32a728e2b3fa6c51b4c63"]
+      url: process.env.ARBITRUM_SEPOLIA_RPC_URL || "https://sepolia-rollup.arbitrum.io/rpc",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
     },
     localhost: {
       url: "http://127.0.0.1:8545"
@@ -32,7 +36,7 @@ const config: HardhatUserConfig = {
   etherscan: {
     // Your API key for Etherscan _
     // Obtain one at https://etherscan.io/ or https://polygonscan.com/
-    apiKey: { arbitrumSepolia: "WZPM4M5GYVTV33XXNXGT7Y1TKDU2R5SJRH" }
+    apiKey: { arbitrumSepolia: process.env.ETHERSCAN_API_KEY || "" }
   }
 };
 
