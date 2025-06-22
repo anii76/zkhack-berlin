a5a1aaca4aa10355144f078567b069719e87c523
anfal.bourouina <anfal.bourouina@qonto.com>
2025-06-22 06:19:50 +0200

    enhance smart contracts

--- backend/contracts/ZFace.sol
+++ backend/contracts/ZFace.sol
@@ -5,8 +5,8 @@ import {HonkVerifier} from "../noir/target/zface_verifier.sol";
 
 contract ZFace {
     HonkVerifier public verifier;
-    bytes32[128] public faceEncoding;
-    bytes32 public threshold;
+    bytes32[128] faceEncoding;
+    bytes32 threshold;
 
     constructor(address _verifier, bytes32[128] memory _faceEncoding, bytes32 _threshold) {
         verifier = HonkVerifier(_verifier);
@@ -16,7 +16,7 @@ contract ZFace {
 
     function verify(
         bytes calldata proof
-    ) external view returns (bool) {
+    ) public view returns (bool) {
         // Public inputs: 128 face encoding values + 1 threshold
         bytes32[] memory publicInputs = new bytes32[](129);
         for (uint256 i = 0; i < 128; i++) {
